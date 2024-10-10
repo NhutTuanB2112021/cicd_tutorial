19bd79a9d50ea1ce962b3378d5cf9b7c89d5a7b0
NhutTuanB2112021 <tuanb2112021@student.ctu.edu.vn>
2024-10-10 12:29:33 +0700

    second commit

--- flask_docker.py
+++ flask_docker.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 
 def hello_world():
-    return 'Hello FOSS'
+    return 'Hello FOSS, Hello CI/CD using Jenkins'
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
